2585b82897ea21b49a39768f59eeffb57566ac93
Robert Spears <robertspears81@gmail.com>
2024-06-05 20:07:47 -0400

    Create Project
    Add Unity Files

---
+++ Assets/_Game/Scripts/RPGAnimationListener.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RHS 
+{
+    public class RPGAnimationListener : MonoBehaviour
+    {
+        public void Hit()
+        {
+        }
+
+        public void Shoot()
+        {
+        }
+
+        public void FootR()
+        {
+        }
+
+        public void FootL()
+        {
+        }
+
+        public void Land()
+        {
+        }        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# SimpleSintyTest1
-More Mountain TDE Project with Synty art assets and Explosive animations
+# SimpleSyntyTest1
+A Unity game project.
+More Mountains TopDown Engine, Synty art assets, and Explosive animations.
+Using Progression System extension for TDE, by Alexander Gheorghe
+https://github.com/reunono/TopDownEngineExtensions/tree/master/ProgressionSystem
